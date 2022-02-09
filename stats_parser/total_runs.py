eabd48e254ced07c9017a5d0c9a8f8e066f1f51c
Mischa <mischavandenburg@gmail.com>
2022-02-09 19:46:10 +0100

    implemented fstrings

--- stats_parser/total_runs.py
+++ stats_parser/total_runs.py
@@ -2,13 +2,14 @@ from pathlib import Path, PurePath
 import re, os
 
 # path to data files
-source = Path(Path.cwd() / 'data' )
+source = Path(Path.home() / 'python' / 'diablo2' / 'stats_parser' / 'data' )
 
 total_games = 0
 pindle_total = 0
 eldritch_total = 0
 shenk_total = 0
 nihla_total = 0
+total_list = []
 
 regex_games = re.compile(r"Games:")
 regex_dict = {
@@ -27,21 +28,17 @@ for folder_name, sub_folder, file_names in os.walk(source):
                 if regex_games.search(line):
                     g = line.split()
                     total_games += int(g[1])
-                    f = my_file.read()
+                    file_read = my_file.read()
 
                     # check which runs were done by using the regex dict
                     for key in regex_dict:
-                        location = regex_dict[key]
-                        reg = re.compile(location)
+                        reg = re.compile(regex_dict[key])
                         # if there is a match, add the numbers to the total variable
-                        if reg.search(f):
-                            var_name = key + '_total'
-                            globals()[var_name] += int(g[1])
+                        if reg.search(file_read):
+                            globals()[f"{key}_total"] += int(g[1])
 
 # print the result
-print('Total runs: ' + str(total_games))
-print('Pindle runs: ' + str(pindle_total))
-print('Eldritch + Shenk runs: ' + str(eldritch_total))
-print('Nihla runs: ' + str(nihla_total))
-
-
+print(f"Total runs: {total_games}")
+print(f"Pindle runs: {pindle_total}")
+print(f"Eldritch + Shenk runs: {eldritch_total}")
+print(f"Nihla runs: {nihla_total}")
