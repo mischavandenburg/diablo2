9948b7531f971a3cafef4aab8fae9b3cf3d73d3c
Mischa <mischavandenburg@gmail.com>
2022-02-16 19:21:15 +0100

    Adjusted to windows path

--- file_copier/file_copier.py
+++ file_copier/file_copier.py
@@ -1,23 +1,16 @@
 '''
-A simple script that copies the info.log and contents of the stats folder to my Google Drive every minute.
+A simple script that copies the info.log to my Google Drive every minute.
 
 Mischa van den Burg
 www.mischavandenburg.com
 
 '''
 
-from pathlib import Path, PurePath
+from pathlib import Path 
 import time, shutil
 
-
-# log_location = Path(Path.home() / 'bot' / '15feb' / 'botty' / 'info.log')
-log_location = '/Users/mischa/bot/info.log'
-
-# log_destination = Path(/Users/mischa/Google Drive/My Drive/2 - Diablo 2/Stats)
-
-#log_destination = PurePath('Users' / 'mischa' / 'Google Drive' / 'My Drive' / '2 - Diablo 2' / 'Stats')
-
-log_destination = '/Users/mischa/Google Drive/My Drive/2 - Diablo 2/Stats'
+log_location = Path('C:/Users/Mischa/bot/15feb/botty/info.log')
+log_destination = Path('C:/Users/Mischa/Mijn Drive/2 - Diablo 2/Stats')
 
 print('Starting copying. Press Ctrl + C to abort.')
 
@@ -25,3 +18,4 @@ while True:
     shutil.copy(log_location, log_destination)
     print('Copied.')
     time.sleep(60)
+
